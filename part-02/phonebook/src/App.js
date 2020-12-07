6bccacfca6648c9ebd44812f389f9aefa3281440
Gianluca Gippetto <gianluca.gippetto@gmail.com>
2020-12-07 11:31:09 +0100

    [2.11] Phonebook: fetch data from json-server

--- part-02/phonebook/src/App.js
+++ part-02/phonebook/src/App.js
@@ -1,26 +1,25 @@
-import React, { useState } from 'react'
-import './App.css'
-import ContactForm from './ContactForm';
-import ContactListWithFilter from './ContactListWithFilter';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import ContactForm from './ContactForm'
+import ContactListWithFilter from './ContactListWithFilter'
 
 
-const Card = ({ className, children }) => {
-   let classes = 'Card' + (' ' + className || '');
-   return (
-      <div className={classes}>
-         {children}
-      </div>
-   )
-}
+export default function App() {
+   const [state, setState] = useState({ type: "loading" })
+   const [contacts, setContacts] = useState([])
 
-
-const App = () => {
-   const [contacts, setContacts] = useState([
-      { name: 'Arto Hellas', phoneNumber: '040-123456' },
-      { name: 'Ada Lovelace', phoneNumber: '39-44-5323523' },
-      { name: 'Dan Abramov', phoneNumber: '12-43-234345' },
-      { name: 'Mary Poppendieck', phoneNumber: '39-23-6423122' },
-   ])
+   useEffect(() => {
+      axios.get('http://localhost:3001/contacts')
+         .then(resp => {
+            console.log(resp);
+            setContacts(resp.data)
+            setState({ type: "ready" })
+         })
+         .catch(error => setState({
+            type: "error",
+            error: error.message,
+         }))
+   }, [])
 
    const addNewContact = (contact) => {
       let newContacts = [...contacts, contact]
@@ -28,19 +27,43 @@ const App = () => {
       setContacts(newContacts)
    }
 
-   return (
-      <div className="App">
-         <h1 className="header">PhoneBook</h1>
-         <div className="content">
+   let content = null;
+   if (state.type === 'ready') {
+      content = (
+         <>
             <Card className="add-item-card">
                <ContactForm contacts={contacts} onSubmit={addNewContact} />
             </Card>
             <Card className="contact-list-card">
                <ContactListWithFilter contacts={contacts} />
             </Card>
+         </>
+      )
+   }
+   else if (state.type === 'loading') {
+      content = null   // FIXME: put a spinner here
+   }
+   else if (state.type === 'error') {
+      content = <p style={{ color: 'red' }}>{state.error}</p>
+   }
+   else
+      throw new Error('invalid state.type');
+
+   return (
+      <div className="App">
+         <h1 className="header">PhoneBook</h1>
+         <div className="content">
+            {content}
          </div>
       </div >
    )
 }
 
-export default App
+const Card = ({ className, children }) => {
+   let classes = 'Card' + (' ' + className || '')
+   return (
+      <div className={classes}>
+         {children}
+      </div>
+   )
+}
